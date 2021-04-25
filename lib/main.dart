5fcd77fa138ffb31acd4c41d6438e23e61b6c243
MohamedMagdy109 <mohamed.magdya109@gmail.com>
2021-04-25 17:30:24 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/todoui.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TODO",
+      debugShowCheckedModeBanner: false,
+      home: todoui(),
+    );
+  }
+}
